5b111fd82b02988c4cdc8afea098bb712a3b273b
Brady <bradytwiz@gmail.com>
2025-09-23 18:54:42 -0700

    Setting up the initial api call and user form

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import axios from "axios";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,6 +16,25 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+});
+
+app.get("/check", async (req, res) => {
+    const myPokemon = req.query.pokemon.toLowerCase();
+    console.log(myPokemon);
+    try {
+        if(myPokemon === 'charizard' || myPokemon === 'mewtwo' || myPokemon === 'raichu')
+        {   
+            // This needs seperate work since they have 2 diff versions, but this is the api call for it
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega-y/`);
+        } else {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega/`);
+        }
+        const result = response.data;
+        res.send(result);
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.send(`${myPokemon} cannot mega evolve (yet). Maybe soon though!`);
+    }
 });
 
 
