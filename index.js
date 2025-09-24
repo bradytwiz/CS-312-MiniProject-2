e0d8e9f5dfe2a48125a45f4691f800f2f1c5e79f
Brady <bradytwiz@gmail.com>
2025-09-23 21:29:02 -0700

    deciding between not existing, existing without a mega, or existing with a mega

--- index.js
+++ index.js
@@ -18,22 +18,49 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 
+async function dataExists(url) {
+    try {
+        const response = await axios.get(url)
+        return response.data;
+    } catch {
+        return null;
+    }
+}
+
+
 app.get("/check", async (req, res) => {
     const myPokemon = req.query.pokemon.toLowerCase();
     console.log(myPokemon);
     try {
-        if(myPokemon === 'charizard' || myPokemon === 'mewtwo' || myPokemon === 'raichu')
-        {   
-            // This needs seperate work since they have 2 diff versions, but this is the api call for it
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega-y/`);
-        } else {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega/`);
+        const pokemonData = await dataExists(`https://pokeapi.co/api/v2/pokemon/${myPokemon}/`);
+
+        if(!pokemonData) {
+            console.log(`${myPokemon} does not exist`);
+            return res.send(`${myPokemon} does not exist. Maybe soon though!`);
         }
-        const result = response.data;
-        res.send(result);
+
+        const megaData = await dataExists(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega/`);
+
+        if (megaData) {
+            console.log(`${myPokemon} has a mega!`);
+            res.send(megaData)
+        } else {
+            console.log(`${myPokemon} exists but mega does not`);
+            res.send(pokemonData);
+        }
+
+        // if(myPokemon === 'charizard' || myPokemon === 'mewtwo' || myPokemon === 'raichu')
+        // {   
+        //     // This needs seperate work since they have 2 diff versions, but this is the api call for it
+        //     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega-y/`);
+        // } else {
+        //     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-form/${myPokemon}-mega/`);
+        // }
+        
+        
     } catch (error) {
         console.error("Failed to make request:", error.message);
-        res.send(`${myPokemon} cannot mega evolve (yet). Maybe soon though!`);
+        res.send();
     }
 });
 
